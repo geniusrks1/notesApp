9f3c3f24f975b741f38cc08cca8270715f4b4c05
geniusrks1 <gmrohitkumar@gmail.com>
2024-08-23 22:44:32 +0530

    context api created  and wrap with index file

---
+++ src/context/AppcontextProvider.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+export const Appcontext=createContext();
+
+const AppcontextProvider = ({children}) => {
+  const[groups,setGroups]=useState(JSON.parse(localStorage.getItem('groups'))||[]);
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || {});
+  const [selectedGroup, setSelectedGroup] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  useEffect(() => {
+    localStorage.setItem('groups', JSON.stringify(groups));
+}, [groups]);
+
+useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+}, [notes]);
+
+    
+    return (
+    <Appcontext.Provider
+    value={{ groups,
+        setGroups,
+        notes,
+        setNotes,
+        selectedGroup,
+        setSelectedGroup,
+        isPopupOpen,
+        setIsPopupOpen}}
+    >{children}</Appcontext.Provider>
+  )
+}
+
+export default AppcontextProvider;
